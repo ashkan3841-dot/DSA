03ee9cfd50bbbf7310acde11690621e3089e68ec
ashkan3841-dot <ashkan3841@gmail.com>
2026-08-20 17:31:10 +0530

    Time: 0 ms (100%), Space: 43.6 MB (40.55%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -17,7 +17,7 @@ class Solution {
         ListNode slow=dummy;
         ListNode fast=dummy;
 
-        for(int i=0;i<n;i++){
+        for(int i=1;i<=n;i++){
             fast=fast.next;
         }
         while(fast.next!=null){
