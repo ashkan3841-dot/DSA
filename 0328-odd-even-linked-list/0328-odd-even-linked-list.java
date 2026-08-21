f449476759f1da71b913566d8251508a29eef387
ashkan3841-dot <ashkan3841@gmail.com>
2026-08-21 16:35:34 +0530

    Time: 0 ms (100%), Space: 46.2 MB (75.65%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -10,37 +10,53 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        ListNode dummy1=new ListNode(0);
-        ListNode dummy2=new ListNode(1);
-        ListNode temp1=head;
-        ListNode temp2=dummy1;
-        ListNode temp3=dummy2;
+        // ListNode dummy1=new ListNode(0);
+        // ListNode dummy2=new ListNode(1);
+        // ListNode temp1=head;
+        // ListNode temp2=dummy1;
+        // ListNode temp3=dummy2;
 
 
 
-        int size=0;
-        while(temp1!=null){
-            size++;
-            temp1=temp1.next;
+        // int size=0;
+        // while(temp1!=null){
+        //     size++;
+        //     temp1=temp1.next;
+        // }
+        // temp1=head;
+        // for(int i=1;i<=size;i++){
+        //     if(i%2!=0){
+        //         ListNode a=new ListNode(temp1.val);
+        //         temp2.next=a;
+        //         temp2=temp2.next;
+        //         temp1=temp1.next;            
+        //     }
+        //     else{
+        //         ListNode b=new ListNode(temp1.val);
+        //         temp3.next=b;
+        //         temp3=temp3.next;
+        //         temp1=temp1.next; 
+
+        //     }
+        // }
+        // temp2.next=dummy2.next;
+        // return dummy1.next;
+        if(head==null || head.next==null){
+            return head;
         }
-        temp1=head;
-        for(int i=1;i<=size;i++){
-            if(i%2!=0){
-                ListNode a=new ListNode(temp1.val);
-                temp2.next=a;
-                temp2=temp2.next;
-                temp1=temp1.next;            
-            }
-            else{
-                ListNode b=new ListNode(temp1.val);
-                temp3.next=b;
-                temp3=temp3.next;
-                temp1=temp1.next; 
 
-            }
+        ListNode odd=head;
+        ListNode even=head.next;
+        ListNode evenhead=even;
+
+        while(even!=null && even.next!=null){
+            odd.next=even.next;
+            odd=even.next;
+            even.next=odd.next;
+            even=odd.next;
         }
-        temp2.next=dummy2.next;
-        return dummy1.next;
+        odd.next=evenhead;
+        return head;
 
 
         
